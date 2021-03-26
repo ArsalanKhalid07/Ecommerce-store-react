029d6ca3575c534cfc156575ec92e8f37d0b052f
Arsalan Khalid <arsalan.khalid@hyperdatacomputing.com>
2021-03-26 10:57:26 +0500

    add item to cart navbar

--- src/Components/Cart-DropDown/CartDrop-Component.jsx
+++ src/Components/Cart-DropDown/CartDrop-Component.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import CustomButton from "../Custom-Button/Custom-Button-Component";
 import "../Cart-DropDown/CartDrop-Styles.scss";
+import  CartItem from "../cart-item/Cart-Item-Component";
+import {connect} from "react-redux";
 
 
-const CartDrop = () => (
+const CartDrop = ({cartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-item">
-
+        <div className="cart-items">
+            {
+                cartItems.map(
+                    cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    )
+                )
+            } 
         </div>
         <CustomButton>
             Add To Cart
@@ -15,4 +23,8 @@ const CartDrop = () => (
 )
 
 
-export default CartDrop;
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    cartItems
+})  
+
+export default connect(mapStateToProps)(CartDrop);
